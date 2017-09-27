75a6ad4e110196d6b77e98b977ada74720f085d2
Sarmad Bokhari <sarmadhbokhari@gmail.com>
2017-09-27 14:13:40 -0700

    Boilerplate setup

---
+++ src/Poll.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Poll extends Component {
+  render() {
+    return (
+      <div className="container">
+        div.col-md-12
+      </div>
+    )
+  }
+}
+
+export default Poll;
